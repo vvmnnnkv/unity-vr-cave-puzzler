9920051c4f7643ea418d724b12850c4f49628225
Vova Manannikov <vmanannikov@broadsoft.com>
2016-12-29 02:16:42 +0300

    feat,docs: Add UI panels and related documentation
    Scene is updated to have UI panels with buttons.
    Basic game logic is implemented to allow user interact with UI and move around.
    Documentation captured current UI implementation.

---
+++ Assets/Cave/Scripts/GameLogic.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour
+{
+    private enum State
+    {
+        Intro,
+        Started,
+        Inside,
+        Puzzle,
+        Success,
+        End
+    }
+
+    private State _state = State.Intro;
+
+    public GameObject player;
+    public GameObject startUI, playUI, restartUI;
+    public GameObject startPoint, playPoint, restartPoint;
+
+    public bool playerWon = false;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && player.transform.position == playPoint.transform.position)
+        {
+            //puzzleSuccess();
+        }
+    }
+
+    public void startGame()
+    {
+        _state = State.Started;
+        UpdateUI();
+    }
+
+    public void startPuzzle()
+    {
+        _state = State.Puzzle;
+        UpdateUI();
+    }
+
+    public void puzzleSuccess()
+    {
+        _state = State.Success;
+        UpdateUI();
+    }
+
+
+    public void enterCave()
+    {
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", playPoint.transform.position,
+                "time", 2,
+                "easetype", "linear",
+                "oncompletetarget", gameObject,
+                "oncomplete", "_onEnterCave"
+            )
+        );
+    }
+
+    private void _onEnterCave()
+    {
+        _state = State.Inside;
+        UpdateUI();
+    }
+
+    public void resetPuzzle()
+    { //Reset the puzzle sequence
+      //        _state = State.Intro;
+      // player.transform.position = startPoint.transform.position;
+      // UpdateUI();
+        SceneManager.LoadScene(0);
+    }
+
+    public void leaveCave()
+    { 
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", restartPoint.transform.position,
+                "time", 2,
+                "easetype", "linear",
+                "oncompletetarget", gameObject,
+                "oncomplete", "_onLeaveCave"
+            )
+        );
+    }
+
+    private void _onLeaveCave()
+    {
+        _state = State.End;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        bool start = false;
+        bool play = false;
+        bool end = false;
+
+        switch (_state)
+        {
+            case State.Intro:
+                start = true;
+                break;
+
+            case State.Inside:
+                play = true;
+                break;
+
+            case State.End:
+                end = true;
+                break;
+        }
+
+        startUI.SetActive(start);
+        playUI.SetActive(play);
+        restartUI.SetActive(end);
+    }
+}
---
+++ Documentation/Process/Phase2/README.md
@@ -0,0 +1,19 @@
+# Phase 2
+At this point the UI panels are added to the scene and button events are set to close panels.
+Player can move around and interact with UI.
+
+## Intro
+Player reads the game introduction. After that, player can still look around and then enter the cave.
+![VR Cave](Screenshot-1.png?raw=true "VR Cave")
+
+## Hint
+After entering the cave, another text gives a hint about what player should do.
+In our case, player should play Simon Says game with crystals.
+The text gives a hint that crystals are representing a puzzle and that player can interact with them, but does not give a clue how to solve it.
+Clicking OKAY will start the puzzle.
+![VR Cave](Screenshot-2.png?raw=true "VR Cave")
+
+## End
+After the puzzle is solved, the cave exit will be unblocked and player can click on the exit.
+After approaching the exit, the final dialog is presented saying that that is the end of the game and allowing to restart.
+![VR Cave](Screenshot-3.png?raw=true "VR Cave")
